283dc603018e3cf2464b9aa330ed4dbd80b3962d
cresas <137892152+cresas@users.noreply.github.com>
2025-03-12 08:34:07 +0300

    Update 8047.js

--- panel/upload/temp/8047.js
+++ panel/upload/temp/8047.js
@@ -7,7 +7,7 @@
         "headline": "Gazavât Hareketi’nin Başlangıcı",
         "text": "Şeyh Mansur liderliğinde Çeçenistan’da ‘Gazavât Hareketi’ başlamıştır. Dinî ve millî nitelikli bu hareket, bağımsız Kafkasya idealini temel almıştır."
       },
-      "media": {"url": "https://cresas.github.io/zamancizelgesi2/img/1.jpg"}
+      "media": {"url": "img/1.jpg"}
     },
     {
       "start_date": {"year": "1834"},
@@ -15,7 +15,7 @@
         "headline": "Şeyh Şamil’in Liderliği Üstlenmesi",
         "text": "Hamza Bey’in vefatı üzerine Gazavât Hareketi’nin liderliğine Şeyh Şamil geçmiştir."
       },
-      "media": {"url": "https://cresas.github.io/zamancizelgesi2/img/2.jpg"},
+      "media": {"url": "img/2.jpg"},
       "background": {"color": "#333333"}
     },
     {
@@ -24,7 +24,7 @@
         "headline": "Gaspıralı İsmail Bey Tercüman Gazetesini Çıkardı",
         "text": "Rusya’daki Müslüman Türklerin modernleşmesi ve millî bilinçlenmeye katkıda bulunması amacıyla Gaspıralı İsmail Bey, Kırım’da Tercüman adlı gazeteyi yayımlamaya başlamıştır."
       },
-      "media": {"url": "https://cresas.github.io/zamancizelgesi2/img/3.jpg"}
+      "media": {"url": "img/3.jpg"}
     },
     {
       "start_date": {"year": "1903"},
@@ -32,7 +32,7 @@
         "headline": "Üç Tarz-ı Siyaset Makalesi Yazıldı",
         "text": "Yusuf Akçura; Osmanlıcılık, İslamcılık ve Türkçülük gibi fikir akımlarını konu alan Üç Tarz-ı Siyaset adlı makaleyi yazmıştır."
       },
-      "media": {"url": "https://cresas.github.io/zamancizelgesi2/img/4.jpg"}
+      "media": {"url": "img/4.jpg"}
     },
     {
       "start_date": {"year": "1917"},
@@ -40,7 +40,7 @@
         "headline": "Muhtar Başkurt Cumhuriyeti İlan Edildi",
         "text": "İdil-Ural boylarında bulunan Başkurtlar, Rusya Müslümanlarının önemli şahsiyetlerinden Ahmet Zeki Velidî Togan öncülüğünde Muhtar Başkurt Cumhuriyeti’ni ilan etmiştir."
       },
-      "media": {"url": "https://cresas.github.io/zamancizelgesi2/img/5.jpg"}
+      "media": {"url": "img/5.jpg"}
     },
     {
       "start_date": {"year": "1918"},
@@ -48,7 +48,7 @@
         "headline": "Azerbaycan Halk Cumhuriyeti’nin Bağımsızlığı",
         "text": "Azerbaycan Halk Cumhuriyeti’nin bağımsızlığı, Gürcistan’ın Tiflis şehrinde Mehmet Emin Resulzade başkanlığındaki Azerbaycan Millî Şûrası tarafından ilan edilmiştir."
       },
-      "media": {"url": "https://cresas.github.io/zamancizelgesi2/img/6.jpg"}
+      "media": {"url": "img/6.jpg"}
     },
     {
       "start_date": {"year": "1921"},
@@ -56,7 +56,7 @@
         "headline": "Enver Paşa ve Basmacı Hareketi",
         "text": "Enver Paşa, Hokand’da başlayan ve kısa sürede diğer bölgelere de yayılan ‘Basmacı Hareketi’ne katılarak bu ayaklanmanın önderi olmuştur."
       },
-      "media": {"url": "https://cresas.github.io/zamancizelgesi2/img/7.jpg"}
+      "media": {"url": "img/7.jpg"}
     }
   ]
 }
